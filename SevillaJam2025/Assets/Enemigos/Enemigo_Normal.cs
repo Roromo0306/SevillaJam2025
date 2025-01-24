320bcca5cc5e21cf4c53aa297057bc61f90b1f49
Alonso2880 <a80alonso@gmail.com>
2025-01-24 21:08:00 +0100

    a

---
+++ SevillaJam2025/Assets/Enemigos/Enemigo_Normal.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class Enemigo_Normal : MonoBehaviour
+{
+    private float vida = 5f;
+    private float damage = 2f;
+    GameObject jugador, enemigo;
+
+    //Perseguir
+    private float distM = 4;
+    private float velocidad = 3f;
+    private Rigidbody rb;
+    void Start()
+    {
+        enemigo = this.gameObject;
+        jugador = GameObject.Find("jugador");
+    }
+
+    
+    void Update()
+    {
+        if (vida <= 0)
+        {
+            Destroy(enemigo);
+        }
+
+        float dist = Vector3.Distance(jugador.transform.position, enemigo.transform.position);
+        if (dist < distM)
+        {
+            perseguir();
+        }
+        Debug.Log("La vida del enemigo es " + vida);
+
+    }
+
+    private void perseguir()
+    {
+        enemigo.transform.LookAt(jugador.transform.position);
+        transform.position = Vector3.MoveTowards(enemigo.transform.position, jugador.transform.position, velocidad * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject jugadorC = collision.gameObject;
+
+        if (jugadorC.tag == "Player")
+        {
+            ataque(jugador);
+        }
+    }
+
+    void ataque(GameObject jugador)
+    {
+        Jugador Jscript = jugador.GetComponent<Jugador>();
+        if (Jscript != null)
+        {
+            Jscript.RecibirDaño(damage);
+        }
+    }
+}
