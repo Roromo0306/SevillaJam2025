55fd23e1c08bd3a85fbe0de2e66e34c36d1f321f
Alonso2880 <a80alonso@gmail.com>
2025-01-25 16:36:36 +0100

    .

--- SevillaJam2025/Assets/Scripts/Boss.cs
+++ SevillaJam2025/Assets/Scripts/Boss.cs
@@ -1,16 +1,61 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Boss : MonoBehaviour
 {
-   
+
+    private float damage = 2f;
+    public GameObject jugador, enemigo;
+
+    //Perseguir
+    private float distM = 4;
+    private float velocidad = 3f;
+    private Rigidbody rb;
     void Start()
     {
-        
+        enemigo = this.gameObject;
+        jugador = GameObject.Find("Player");
     }
 
 
     void Update()
     {
-        
+        if (VidaEnemigos.Vida_Normal <= 0)
+        {
+            Destroy(enemigo);
+        }
+
+        float dist = Vector3.Distance(jugador.transform.position, enemigo.transform.position);
+        if (dist < distM)
+        {
+            perseguir();
+        }
+        // Debug.Log("La vida del enemigo es " + VidaEnemigos.Vida_Normal);
+
+    }
+
+    private void perseguir()
+    {
+        enemigo.transform.LookAt(jugador.transform.position);
+        transform.position = Vector3.MoveTowards(enemigo.transform.position, jugador.transform.position, velocidad * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject jugadorC = collision.gameObject;
+
+        if (jugadorC.tag == "Player")
+        {
+            ataque(jugador);
+        }
+    }
+
+    void ataque(GameObject jugador)
+    {
+        Player Jscript = jugador.GetComponent<Player>();
+        if (Jscript != null)
+        {
+            Jscript.RecibirDaño(damage);
+        }
     }
 }
