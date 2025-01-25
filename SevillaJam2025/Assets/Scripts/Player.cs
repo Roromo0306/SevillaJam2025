a8a6e36759b806dfa513ad380fa3c2f4b84cd611
Roromo03 <rromeromorera@gmail.com>
2025-01-25 20:01:38 +0100

    me cago

--- SevillaJam2025/Assets/Scripts/Player.cs
+++ SevillaJam2025/Assets/Scripts/Player.cs
@@ -134,17 +134,22 @@ public class Player : MonoBehaviour
 
     }
 
+    public float direccionSigno()
+    {
+        if(CoordX < 0)
+        {
+            return -1;
+        }
+        else
+        {
+            return 1;
+        }
+    }
     private void Awake()
     {
-        
-        
+    
             Instance = this;
             DontDestroyOnLoad(transform.parent.gameObject);
-        
-       
-
-
-
     }
     private IEnumerator Dash()
     {
