72a2b7166575f731f74dadab7e2f22c2bb10f4f5
lauramun23 <lauramunoz.23@campuscamara.es>
2025-01-25 23:49:44 +0100

    final salas

--- SevillaJam2025/Assets/Scripts/CamaraSeguimiento.cs
+++ SevillaJam2025/Assets/Scripts/CamaraSeguimiento.cs
@@ -4,16 +4,25 @@ public class CamaraSeguimiento : MonoBehaviour
 {
     Transform player; // Referencia al jugador
     public float height = 5.55f; // Altura fija de la cámara
+    private float fixedX; // Guarda la posición X fija de la cámara
+
 
 
     private void Start()
     {
         player = Player.Instance.transform;
+        fixedX = player.position.x; 
+
     }
 
     private void LateUpdate()
     {
-        // La cámara sigue al jugador, pero solo en X y Z, manteniendo la altura fija
-        transform.position = new Vector3(player.position.x, height, player.position.z - 15.77f);
+        transform.position = new Vector3(fixedX, height, player.position.z - 12f);
+    }
+
+    public void UpdateCameraPosition(Vector3 newRoomPosition)
+    {
+        fixedX = newRoomPosition.x; 
+        transform.position = new Vector3(fixedX, height, newRoomPosition.z + player.position.z - 12f);
     }
 }
--- SevillaJam2025/Assets/Scripts/MovHabitaciones.cs
+++ SevillaJam2025/Assets/Scripts/MovHabitaciones.cs
@@ -3,20 +3,27 @@ using UnityEngine;
 public class MovHabitaciones : MonoBehaviour
 {
     public Transform targetRoom; // La habitación a la que lleva esta puerta
-    Transform player; // Referencia al jugador
+    public Transform centroHabitacion; 
+    Transform player; 
+    CamaraSeguimiento camara;
 
     void Start()
     {
         player = Player.Instance.transform;
+        camara = Camera.main.GetComponent<CamaraSeguimiento>();
     }
 
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player")) // Si el jugador toca la puerta
+        if (other.CompareTag("Player")) 
         {
-            // Mueve al jugador al centro de la nueva habitación
-            player.position = targetRoom.position;
+            Vector3 entrada = targetRoom.position + (targetRoom.forward * 1.5f);
+            player.position = entrada;
+
+            // Ajusta la cámara a la posición de la nueva habitación
+            camara.UpdateCameraPosition(centroHabitacion.position);
+
         }
     }
 }
