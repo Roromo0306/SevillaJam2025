50e4aecc1fc44b541bd464993fdff823c46756c3
Alonso2880 <a80alonso@gmail.com>
2025-01-26 10:35:21 +0100

    .

---
+++ SevillaJam2025/Assets/Scripts/BalaBoss.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class BalaBoss : MonoBehaviour
+{
+    private float damage = 1.5f;
+    private GameObject jugador, bala;
+
+    //Perseguir
+    private float velocidad = 3f;
+    private Rigidbody rb;
+    private Vector3 direccion;
+
+    private float tiempInicial = 0f;
+    private float tiempFinal = 3500f;
+    void Start()
+    {
+        bala = this.gameObject;
+        jugador= GameObject.FindGameObjectWithTag("PlayerVerdadero");
+
+        direccion = (jugador.transform.position - transform.position).normalized;
+    }
+
+    
+    void Update()
+    {
+        transform.position += direccion * velocidad * Time.deltaTime;
+
+        temporizador();
+        Debug.Log("El tiempo inicial es " + tiempInicial);
+    }
+
+    private void temporizador()
+    {
+        tiempInicial++;
+        if (tiempInicial == tiempFinal)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject jugadorC = collision.gameObject;
+        if (jugador.tag == "PlayerVerdadero")
+        {
+            ataque(jugador);
+        }
+    }
+
+    private void ataque(GameObject jugador)
+    {
+        Player Jscript = jugador.GetComponent<Player>();
+
+        Jscript.vida = Jscript.vida - 1;
+        Destroy(this.gameObject);
+    }
+}
